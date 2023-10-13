7f6e6199ede8e812465fd34c5449ce97f5877cd3
Samuel Taggart <sam@sasworkshops.com>
2023-10-13 11:38:52 -0600

    . e Added error handling to bash script
    Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,7 @@
+#! /usr/bin/bash
+
+set -euo pipefail
+
+
 g-cli vipc -- -v "20.0 (64-bit)" "Approvals Starter Project.vipc"
 g-cli "C:\Program Files\National Instruments\LabVIEW 2020\vi.lib\addons\_JKI Toolkits\Caraya\CarayaCLIExecutionEngine.vi" -- -s "C:\Users\SAS\Documents\GitHub\ApprovalTests.LabVIEW.StarterProject" -x "C:\Users\SAS\Documents\GitHub\ApprovalTests.LabVIEW.StarterProject\UnitTestReport.xml"
