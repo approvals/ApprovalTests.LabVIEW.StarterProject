e4ae86d37a64d738ec0c3ecfc2dd8e8726d8ef49
Samuel Taggart <sam@sasworkshops.com>
2023-10-13 16:17:30 -0600

    . e Added timeout to vipc applier.

--- run_test.sh
+++ run_test.sh
@@ -8,5 +8,5 @@ echo "CURRENT_DIRECTORY = $CURRENT_DIRECTORY"
 CARAYA="C:\Program Files\National Instruments\LabVIEW 2020\vi.lib\addons\_JKI Toolkits\Caraya\CarayaCLIExecutionEngine.vi"
 echo "CARAYA = $CARAYA"
 
-g-cli vipc -- -v "20.0 (64-bit)" "$CURRENT_DIRECTORY\Approvals Starter Project.vipc"
+g-cli vipc -- -v "20.0 (64-bit)" -t 1200 "$CURRENT_DIRECTORY\Approvals Starter Project.vipc"
 g-cli "$CARAYA" -- -s "$CURRENT_DIRECTORY" -x "$CURRENT_DIRECTORY\UnitTestReport.xml"
