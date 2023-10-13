212a09885732315a28973cc742ca2a0d283520bd
Samuel Taggart <sam@sasworkshops.com>
2023-10-13 11:32:01 -0600

    . e Added load packages
    Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>

--- run_test.sh
+++ run_test.sh
@@ -1,1 +1,2 @@
+g-cli vipc -- -v "20.0 (64-bit)" "Approvals Starter Project.vipc"
 g-cli "C:\Program Files\National Instruments\LabVIEW 2020\vi.lib\addons\_JKI Toolkits\Caraya\CarayaCLIExecutionEngine.vi" -- -s "C:\Users\SAS\Documents\GitHub\ApprovalTests.LabVIEW.StarterProject" -x "C:\Users\SAS\Documents\GitHub\ApprovalTests.LabVIEW.StarterProject\UnitTestReport.xml"
