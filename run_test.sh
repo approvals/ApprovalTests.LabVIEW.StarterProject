440e6a7f0a298832094513e4da8f5e62ea7d2b71
Samuel Taggart <sam@sasworkshops.com>
2023-10-17 16:33:36 -0600

    . e Simplified bash script by using relative paths
    Left current directory as $HERE - might be useful...

--- run_test.sh
+++ run_test.sh
@@ -2,11 +2,7 @@
 
 set -euo pipefail
 
-CURRENT_DIRECTORY=$(cygpath -w $(pwd))
-echo "CURRENT_DIRECTORY = $CURRENT_DIRECTORY"
+HERE=$(cygpath -w $(pwd))
 
-CARAYA="C:\Program Files\National Instruments\LabVIEW 2020\vi.lib\addons\_JKI Toolkits\Caraya\CarayaCLIExecutionEngine.vi"
-echo "CARAYA = $CARAYA"
-
-g-cli vipc -- -v "20.0 (64-bit)" -t 1200 "$CURRENT_DIRECTORY\Approvals Starter Project.vipc"
-g-cli "$CARAYA" -- -s "$CURRENT_DIRECTORY" -x "$CURRENT_DIRECTORY\UnitTestReport.xml"
+g-cli vipc -- -v "20.0 (64-bit)" -t 1200 "Approvals Starter Project.vipc"
+g-cli caraya -- -s . -x "UnitTestReport.xml"
